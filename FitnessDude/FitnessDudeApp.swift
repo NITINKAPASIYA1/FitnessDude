829e4baafac06e9a3a3a5fc7131abd6e634e5fbb
Nitin Kumar <kumarnitin5814@gmail.com>
2025-02-10 01:44:51 +0530

    Initial Commit

---
+++ FitnessDude/FitnessDudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessDudeApp.swift
+//  FitnessDude
+//
+//  Created by Nitin on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessDudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
